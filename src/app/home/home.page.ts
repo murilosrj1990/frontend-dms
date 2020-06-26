b957ff680714e699f0678bca84cc2c3da7f90061
set <set>
2020-06-25 23:40:24 -0300

    error handling home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -12,15 +13,40 @@ export class HomePage {
   public email:string;
   public senha:string;
 
-  constructor(private router:Router,private auth:AuthService) {}
+  constructor(
+    private router:Router,
+    private auth:AuthService,
+    private loadingCtrl:LoadingController,
+    private toastCtrl:ToastController) {}
 
   async entrar(){
+    
+    const loading = await this.loadingCtrl.create({
+      message: 'Acessando sistema...'
+    });
+    await loading.present();
     this.auth.authToken(this.email,this.senha).then((resolve:any)=>{
+      loading.dismiss();
+      this.showToastMessenger("Bem vindo "+resolve.user.name);
       this.router.navigateByUrl('options');
-    })
-    
-    
-
+    }).catch((error)=>{
+      if(error.status===0){
+        this.showToastMessenger("Erro desconhecido!");
+        loading.dismiss();
+      }
+      if(error.status===400){
+        this.showToastMessenger("Usuário não encontrado!");
+        loading.dismiss();
+      }
+      loading.dismiss();
+    });
   }
 
+  async showToastMessenger(message){
+    const toast = await this.toastCtrl.create({
+      message,
+      duration: 2000
+    });
+    toast.present();
+  }
 }
